82598202830c5da006fbaf5fa2fa2e0a53b71a78
samuelvoltarius <73832529+samuelvoltarius@users.noreply.github.com>
2025-07-12 00:47:01 +0200

    Add INDI PPA to Astro install

--- astro_install.sh
+++ astro_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# Ensure PHD2 packages are available
+sudo add-apt-repository -y ppa:pch/phd2
+# PHD2 depends on INDI packages from the mutlaqja PPA
+sudo add-apt-repository -y ppa:mutlaqja/ppa
 sudo apt update
 sudo apt install -y kstars phd2 siril gphoto2 entangle
 sudo apt install -y flatpak
