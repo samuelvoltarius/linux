ed2367675737ee01de4977a7d86f9b18dd145342
samuelvoltarius <73832529+samuelvoltarius@users.noreply.github.com>
2025-07-12 00:58:41 +0200

    Improve Kali install script with signed key

--- kali_install.sh
+++ kali_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" | sudo tee /etc/apt/sources.list.d/kali.list
-wget -q -O - https://archive.kali.org/archive-key.asc | sudo apt-key add -
+echo "deb [signed-by=/usr/share/keyrings/kali-archive-keyring.gpg] http://http.kali.org/kali kali-rolling main non-free contrib" | sudo tee /etc/apt/sources.list.d/kali.list
+curl -fsSL https://archive.kali.org/archive-key.asc | sudo gpg --dearmor -o /usr/share/keyrings/kali-archive-keyring.gpg
 sudo apt update
 sudo apt install -y kali-linux-core kali-system-gui kali-linux-headless kali-tools-top10 kali-linux-default \
     kali-tools-information-gathering kali-tools-vulnerability-analysis kali-tools-web kali-tools-exploitation \
