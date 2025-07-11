bb845941bc0db427b0aafc0b9d1c972cb756ca78
samuelvoltarius <73832529+samuelvoltarius@users.noreply.github.com>
2025-07-12 00:40:11 +0200

    Add PPA for PHD2 in astro_install

--- astro_install.sh
+++ astro_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+# Ensure PHD2 packages are available
+sudo add-apt-repository -y ppa:pch/phd2
 sudo apt update
 sudo apt install -y kstars phd2 siril gphoto2 entangle
 sudo apt install -y flatpak
